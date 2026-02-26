32ddee1135f3f8416e8f8ae2525ad3e1700a8ad2
Olaygold <olayinka10172007@gmail.com>
2026-02-26 07:50:14 +0100

    Update ReferralEarning.js

--- server/models/ReferralEarning.js
+++ server/models/ReferralEarning.js
@@ -1,3 +1,4 @@
+
 // models/ReferralEarning.js
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
@@ -10,40 +11,55 @@ const ReferralEarning = sequelize.define('ReferralEarning', {
   },
   referrerId: {
     type: DataTypes.UUID,
-    allowNull: false
+    allowNull: false,
+    comment: 'The user who referred and earned the commission'
   },
   referredUserId: {
     type: DataTypes.UUID,
-    allowNull: false
+    allowNull: false,
+    comment: 'The user who was referred and placed the bet'
   },
   betId: {
     type: DataTypes.UUID,
-    allowNull: true
+    allowNull: true,
+    comment: 'The bet that triggered this earning'
   },
   type: {
     type: DataTypes.ENUM('first_bet', 'loss_commission'),
-    allowNull: false
+    allowNull: false,
+    comment: 'first_bet = normal referrer (5% once), loss_commission = influencer (X% every loss)'
   },
   percentage: {
     type: DataTypes.DECIMAL(5, 2),
-    allowNull: false
+    allowNull: false,
+    comment: 'The percentage used to calculate commission'
   },
   betAmount: {
     type: DataTypes.DECIMAL(15, 2),
-    allowNull: false
+    allowNull: false,
+    comment: 'The original bet amount'
   },
   earnedAmount: {
     type: DataTypes.DECIMAL(15, 2),
-    allowNull: false
+    allowNull: false,
+    comment: 'The commission earned (betAmount * percentage / 100)'
   },
   status: {
     type: DataTypes.ENUM('pending', 'credited', 'withdrawn'),
-    defaultValue: 'credited'
+    defaultValue: 'credited',
+    comment: 'pending = awaiting, credited = added to balance, withdrawn = moved to wallet'
   }
 }, {
   timestamps: true,
   tableName: 'referral_earnings',
-  underscored: false
+  underscored: false,
+  indexes: [
+    { fields: ['referrerId'] },
+    { fields: ['referredUserId'] },
+    { fields: ['betId'] },
+    { fields: ['type'] },
+    { fields: ['createdAt'] }
+  ]
 });
 
 module.exports = ReferralEarning;
